11be192729fc0d57c3cd808ae8154aad6d703a0b
Jordan Walker <jordanswalker28@gmail.com>
2023-02-24 01:16:50 +0000

    Added popup window

--- script.js
+++ script.js
@@ -137,3 +137,69 @@ function createItem(item) {
     // Add the timeline item to the container
     timelineContainer.appendChild(timelineItem);
 }
+
+
+// Make the window draggable
+dragElement(document.getElementById("myWindow"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "-header")) {
+        // If present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "-header").onmousedown = dragMouseDown;
+    } else {
+        // Otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // Call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // Calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // Set the element's new position:
+        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+    }
+
+    function closeDragElement() {
+        // Stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
+
+// Close the window when the user clicks on the X button
+function closeWindow() {
+    var window = document.getElementById("myWindow");
+    window.style.display = "none";
+}
+
+// Show the window when the user clicks the show button
+function showWindow() {
+    var window = document.getElementById("myWindow");
+    window.style.display = "block";
+
+    // Listen for clicks on the window object
+    window.addEventListener("click", function (event) {
+        // Check if the clicked element is inside the custom window or not
+        if (!window.contains(event.target) && window.style.display !== "none") {
+        closeWindow();
+        }
+    });
+}
+
