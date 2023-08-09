dbd6126b9b15f32901a5b8f386c6065ddc17adce
Jordan Walker <jordanswalker28@gmail.com>
2023-08-09 21:50:25 +0100

    Made responsive navbar

--- my-website/src/components/Navbar.js
+++ my-website/src/components/Navbar.js
@@ -2,14 +2,23 @@
 // Navbar.js 
 import "../styles/navbar.css"
 
+import { useState } from "react"
+import "../styles/navbar.css"
+
 export default function Navbar() {
-    return (
-      <nav className="navigation">
-        <a href="/" className="brand-name">
-          MacroSoft
-        </a>
-        <button className="hamburger">
-          {/* icon from heroicons.com */}
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+
+  return (
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        MacroSoft
+      </a>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded)
+        }}
+      >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="h-5 w-5"
@@ -21,22 +30,24 @@ export default function Navbar() {
               d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
               clipRule="evenodd"
             />
-          </svg>
-        </button>
-        <div
-          className="navigation-menu">
-          <ul>
-            <li>
-              <a href="/home">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+          </svg>      </button>
+      <div
+        className={
+          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul>
+          <li>
+            <a href="/home">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
