10bcc9148c1057c659c7b7872da8c2efb241603c
Jordan Walker <jordanswalker28@gmail.com>
2023-08-13 17:02:21 +0100

    Added Links To Nav

--- my-website/src/components/Navbar.js
+++ my-website/src/components/Navbar.js
@@ -39,13 +39,13 @@ export default function Navbar() {
       >
         <ul>
           <li>
-            <a href="/home">Home</a>
+            <Link to="/">Home</Link>
           </li>
           <li>
-            <a href="/about">About</a>
+            <Link to="/about">About</Link>
           </li>
           <li>
-            <a href="/contact">Contact</a>
+            <Link to="/posts">Posts</Link>
           </li>
         </ul>
       </div>
