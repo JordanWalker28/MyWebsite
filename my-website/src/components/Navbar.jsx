2518efe261dafcddba57b670acc70a6f83ef5a5e
Jordan Walker <jordanswalker28@gmail.com>
2023-08-15 21:13:24 +0100

    refactor navbar

--- my-website/src/components/Navbar.jsx
+++ my-website/src/components/Navbar.jsx
@@ -1,61 +1,63 @@
- 
-// Navbar.js 
-import { NavLink } from 'react-router-dom';
-import "../styles/navbar.css"
-import React from "react"
-import { useState } from "react"
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import '../styles/navbar.css';
 
 export default function Navbar() {
-  const [isNavExpanded, setIsNavExpanded] = useState(false)
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
   const closeMenu = () => {
     setIsNavExpanded(false);
   };
 
+  const toggleMenu = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
 
   return (
     <nav className="navigation">
-      <NavLink activeclassname="none" to="/" className="brand-name" onClick={closeMenu}>
+      <Link to="/" className="brand-name" onClick={closeMenu}>
         JordanW
-      </NavLink>
-      <button
-        className="hamburger"
-        onClick={() => {
-          setIsNavExpanded(!isNavExpanded)
-        }}
-      >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="white"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>      </button>
-      <div
-        className={
-          isNavExpanded ? "navigation-menu expanded" : "navigation-menu"
-        }
-      >
+      </Link>
+      <button className="hamburger" onClick={toggleMenu}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div className={`navigation-menu ${isNavExpanded ? 'expanded' : ''}`}>
         <ul>
           <li>
-            <NavLink activeclassname="active" to="/projects" onClick={closeMenu}>Projects</NavLink>
+            <NavLink activeClassName="active" to="/projects" onClick={closeMenu}>
+              Projects
+            </NavLink>
           </li>
           <li>
-            <NavLink activeclassname="active" to="/posts" onClick={closeMenu}>Posts</NavLink>
+            <NavLink activeClassName="active" to="/posts" onClick={closeMenu}>
+              Posts
+            </NavLink>
           </li>
           <li>
-            <NavLink activeclassname="active" to="/resume" onClick={closeMenu}>Resume</NavLink>
+            <NavLink activeClassName="active" to="/resume" onClick={closeMenu}>
+              Resume
+            </NavLink>
           </li>
           <li>
-            <NavLink activeclassname="active" to="/about" onClick={closeMenu}>About</NavLink>
+            <NavLink activeClassName="active" to="/about" onClick={closeMenu}>
+              About
+            </NavLink>
           </li>
           <li>
-            <NavLink activeclassname="active" to="/contact" onClick={closeMenu}>Contact</NavLink>
+            <NavLink activeClassName="active" to="/contact" onClick={closeMenu}>
+              Contact
+            </NavLink>
           </li>
         </ul>
       </div>
