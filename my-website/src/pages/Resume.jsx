983cf7c6ee6c1d50bba786dfdd30902839951beb
Jordan Walker <jordanswalker28@gmail.com>
2023-08-27 18:35:43 +0100

    Added filter for education

--- my-website/src/pages/Resume.jsx
+++ my-website/src/pages/Resume.jsx
@@ -5,15 +5,18 @@ import data from '../json/WorkHistory.json';
 
 function Resume({ title }) {
   const [searchValue, setSearchValue] = useState('');
+  const [filterType, setFilterType] = useState('All'); // Added state for filter type
 
   const lowercaseSearchValue = searchValue.toLowerCase();
 
-  // Filtered data based on search value
-  const filteredData = data.filter(item => 
-    item.companyName.toLowerCase().includes(lowercaseSearchValue)
-  );
+  // Filtered data based on search value and filter type
+  const filteredData = data.filter(item => {
+    const companyNameMatches = item.companyName.toLowerCase().includes(lowercaseSearchValue);
+    const typeMatches = filterType === 'All' || (filterType === 'Education' && item.eductation) || (filterType === 'Employment' && !item.eductation);
+    return companyNameMatches && typeMatches;
+  });
 
-  const isFiltered = lowercaseSearchValue !== '';
+  const isFiltered = lowercaseSearchValue !== '' || filterType !== 'All';
 
   return (
     <Page title={title}>
@@ -25,6 +28,16 @@ function Resume({ title }) {
           value={searchValue}
           onChange={e => setSearchValue(e.target.value)}
         />
+        <label htmlFor="filter-type">Filter by:</label>
+        <select
+          id="filter-type"
+          value={filterType}
+          onChange={e => setFilterType(e.target.value)}
+        >
+          <option value="All">All</option>
+          <option value="Education">Education</option>
+          <option value="Employment">Employment</option>
+        </select>
       </div>
       <div className={`timeline ${isFiltered ? 'filtered' : ''}`}>
         {filteredData.length > 0 ? (
@@ -33,7 +46,8 @@ function Resume({ title }) {
               key={index}
               item={item}
               className={`timeline-item ${isFiltered ? 'slide-in' : 'slide-out'}`}
-            />          ))
+            />
+          ))
         ) : (
           <p>No matching items found.</p>
         )}
