67c136fca9dddbfbfdcc905a410819560def01a5
Jordan Walker <jordanswalker28@gmail.com>
2023-08-28 21:08:30 +0100

    Changes to contact page

--- my-website/src/pages/Contact.jsx
+++ my-website/src/pages/Contact.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import Page from '../components/Page';
+import '../styles/contact.css';
 
 function Contact({title}) {
   return (
     <Page title={title}>
-        <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+      <div class="center-container">
+
+  <div class="grid-container">
+    <a href="https://github.com">
+      <img src="github-icon.png" alt="GitHub"></img>
+    </a>
+    <a href="https://twitter.com">
+      <img src="twitter-icon.png" alt="Twitter"></img>
+    </a>
+    <a href="mailto:your.email@gmail.com">
+      <img src="gmail-icon.png" alt="Gmail"></img>
+    </a>
+    <a href="https://linkedin.com">
+      <img src="linkedin-icon.png" alt="LinkedIn"></img>
+    </a>
+  </div>
+  </div>
     </Page>
   );
   }
