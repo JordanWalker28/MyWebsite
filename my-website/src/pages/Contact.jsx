807e42b1a619cb3fe3ec2ca7d1934dce862e6743
Jordan Walker <jordanswalker28@gmail.com>
2023-09-02 22:39:34 +0100

    Updated social items to come from json

--- my-website/src/pages/Contact.jsx
+++ my-website/src/pages/Contact.jsx
@@ -1,29 +1,24 @@
 import React from 'react';
 import Page from '../components/Page';
 import '../styles/contact.css';
+import socialLinksData from '../json/Social.json';
 
-function Contact({title}) {
+function Contact({ title }) {
+  const socialLinks = socialLinksData;
+
   return (
     <Page title={title}>
-      <div class="center-container">
-
-  <div class="grid-container">
-    <a href="https://github.com">
-      <img src="github-icon.png" alt="GitHub"></img>
-    </a>
-    <a href="https://twitter.com">
-      <img src="twitter-icon.png" alt="Twitter"></img>
-    </a>
-    <a href="mailto:your.email@gmail.com">
-      <img src="gmail-icon.png" alt="Gmail"></img>
-    </a>
-    <a href="https://linkedin.com">
-      <img src="linkedin-icon.png" alt="LinkedIn"></img>
-    </a>
-  </div>
-  </div>
+      <div className="center-container">
+        <div className="grid-container">
+          {socialLinks.map((link, index) => (
+            <a key={index} href={link.URL}>
+              <img src={link.Image} alt={link.Name}></img>
+            </a>
+          ))}
+        </div>
+      </div>
     </Page>
   );
-  }
+}
 
 export default Contact;
