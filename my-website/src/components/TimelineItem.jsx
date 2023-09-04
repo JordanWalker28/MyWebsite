4cfa652900c60cd18bf8a0a62945bbe7a3eb872b
Jordan Walker <jordanswalker28@gmail.com>
2023-09-04 23:54:11 +0100

    Updated the css

--- my-website/src/components/TimelineItem.jsx
+++ my-website/src/components/TimelineItem.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import '../styles/timeline.css'; 
+import React, { useState } from 'react';
+import '../styles/timeline.css';
 
 function TimelineItem({ item }) {
-    return (
-      <div class="business-card">
-				<div class="profile">
-					<div class="profile-image">
-          <img src={process.env.PUBLIC_URL + '/' + item.companyLogo} alt={item.companyName} />
-          </div>
-					<div class="profile-title">
-          <h3>{item.education ? 'education' : 'professional'} - {item.companyName}</h3>						
-          <h4>{`${item.startDate} - ${item.endDate}`}</h4>
-					<span><a href={item.websiteUrl}>{item.companyName}</a></span>
-					</div>
-				</div>		
-			</div>
-    );
-  }
+  const [isOpen, setIsOpen] = useState(false);
 
-  export default TimelineItem;
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className={`business-card ${isOpen ? 'open' : ''}`}>
+      <div className="profile">
+        <div className="profile-image">
+          <img src={process.env.PUBLIC_URL + '/' + item.companyLogo} alt={item.companyName} />
+        </div>
+        <div className="profile-title">
+          <h3>{item.education ? 'education' : 'professional'} - {item.companyName}</h3>
+          <h4>{`${item.startDate} - ${item.endDate}`}</h4>
+          <span><a href={item.websiteUrl}>{item.companyName}</a></span>
+        </div>
+        <details open={isOpen}>
+          <summary onClick={toggleAccordion}>Show More</summary>
+          <div className="accordion-content">
+            <p>{item.description}</p>
+          </div>
+        </details>
+      </div>
+    </div>
+  );
+}
+
+export default TimelineItem;
