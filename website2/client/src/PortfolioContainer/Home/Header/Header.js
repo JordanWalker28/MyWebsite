f54f527cc0d208e90ac190b540beeaf8c0da1c24
Jordan Walker <jordanswalker28@gmail.com>
2024-11-19 00:21:15 +0000

    remove unneeded naming

--- website2/client/src/PortfolioContainer/Home/Header/Header.js
+++ website2/client/src/PortfolioContainer/Home/Header/Header.js
@@ -70,7 +70,6 @@ export default function Header() {
           <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
         </div>
         <div className="header-logo">
-          <span>Jordan</span>
         </div>
         <div
           className={
